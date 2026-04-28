5e7b509a879a0e40964cc0ece4e8d305e9db073d
albesroger <albescaballero@gmail.com>
2026-04-28 14:35:02 -0400

    feat: informacion de ususario autenticado

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,6 +2,8 @@ from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from core.models import Owner
 
 
 class RegisterView(APIView):
@@ -13,3 +15,29 @@ class RegisterView(APIView):
             serializer.save()
             return Response({'message': 'Registro exitoso.'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CurrentUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        try:
+            owner = user.owner_profile
+            owner_data = {
+                'id': owner.id,
+                'phone': owner.phone,
+                'address': owner.address,
+            }
+        except Owner.DoesNotExist:
+            owner_data = None
+
+        data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'owner': owner_data,
+        }
+        return Response(data)
